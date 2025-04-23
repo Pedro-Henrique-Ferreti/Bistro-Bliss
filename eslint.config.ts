5562d91a5818a87029762b38e54a24f001829638
Pedro Henrique <phsouza@jacksondawson.com>
2025-04-23 11:55:09 -0300

    Setup ESLint configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,26 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
-
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-)
+  vueTsConfigs.recommendedTypeChecked,
+  vueTsConfigs.stylisticTypeChecked,
+  stylistic.configs.customize({
+    semi: true,
+  }),
+  ...pluginVue.configs['flat/recommended'],
+  {
+    rules: {
+      'vue/block-order': ['error', {
+        order: ['template', 'script', 'style'],
+      }],
+      'vue/html-self-closing': 'error',
+    },
+  },
+);
