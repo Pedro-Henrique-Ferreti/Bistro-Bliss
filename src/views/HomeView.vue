28b6e77f340bfddad86825dfbbf1d45b4b8885a3
Pedro Henrique <phsouza@jacksondawson.com>
2025-04-25 13:00:55 -0300

    Created Home hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Hello World</h1>
+  <main>
+    <section class="
+      flex justify-center items-center flex-col h-[35rem] p-(--spacing-screen-padding) text-center
+      bg-[url('/images/hero-background.webp')] bg-cover bg-center desktop:h-[calc(100vh-9.25rem)]
+    ">
+      <h1 class="h1 font-secondary">
+        Best food for <br> your taste
+      </h1>
+      <p class="text-xl mt-2 max-w-[33.5rem]">
+        Discover delectable cuisine and unforgettable moments in our welcoming, culinary haven.
+      </p>
+      <div class="flex flex-wrap justify-center gap-1 mt-[2.5rem]">
+        <AppButton>Book A Table</AppButton>
+        <AppButton variant="outline">
+          Explore Menu
+        </AppButton>
+      </div>
+    </section>
+  </main>
 </template>
+
+<script setup lang="ts">
+import AppButton from '@/components/AppButton.vue';
+</script>
