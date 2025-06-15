eba6a2ec8b5d47371f379e386c3a2b7d62996d14
Pedro Henrique <phsouza@jacksondawson.com>
2025-06-15 17:42:12 -0300

    Install TypeScript and configure type-check command

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1>Hello world</h1>
+  <button type="button" @click="count++">
+    Count is: {{ count }}
+  </button>
 </template>
+
+<script lang="ts" setup>
+const count = ref<number>(0);
+</script>
