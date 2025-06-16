6968d68602bb22ade2358f9ba52047019033105f
Pedro Henrique <phsouza@jacksondawson.com>
2025-06-16 17:27:04 -0300

    Add Tailwind and CSS stylesheets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
 	modules: ['@nuxt/eslint'],
 	devtools: { enabled: true },
+	css: ['@/assets/css/main.css'],
 	devServer: {
 		port: 8080,
 	},
 	compatibilityDate: '2025-05-15',
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	eslint: {
 		checker: true,
 		config: {
