3f5d2d2bb8a96c9e0c6ad2e93cc0e3a09070ea94
Pedro Henrique <phsouza@jacksondawson.com>
2025-06-16 17:43:05 -0300

    Add HTML link elements for font stylesheets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,18 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
 	modules: ['@nuxt/eslint'],
 	devtools: { enabled: true },
+	app: {
+		head: {
+			link: [
+				{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+				{ rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+				{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap' },
+			],
+			meta: [
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			],
+		},
+	},
 	css: ['@/assets/css/main.css'],
 	devServer: {
 		port: 8080,
